7358caf3833a049204f7b52569ecf60da2c235a5
libailing <calipos@126.com>
2020-06-05 06:04:37 +0800

    20200605

--- stock.py
+++ stock.py
@@ -15,9 +15,17 @@ if(os.path.exists(dataFolder)):
     print("dataFolder already exist")
 else:os.mkdir('./'+dataFolder)
 
+authStatus = auth('15850798209','rbldevil198929aA')
+#authStatus = auth('13308189942','rbldevil198929aA')
+#authStatus = auth('15730085953','rbldevil198929aA')
+#authStatus = auth('18081550388','rbldevil198929aA')
+print("sdk count = ",get_query_count())
+#all_securities=get_all_securities(['stock'])
+#all_securities.to_csv('stocklist.csv', sep=',')
+all_securities=pd.read_csv('stocklist.csv', sep=',',index_col=[0])
 
 potentialList={}  
-potentialRate=[-0.025,-0.019]
+potentialRate=[-0.11,0.11]
 
 quotation = easyquotation.use('sina')
 snapshot = quotation.market_snapshot(prefix=True)
@@ -38,45 +46,39 @@ for code in codes:
     if currentDayAvg<lastDayClose:
         codeHead = code[:2]
         codeNum = code[2:]
-        if codeNum[:3]=='300':
-            continue
-        if codeNum[:3]=='200':
-            continue
-        if codeNum[:3]=='900':
-            continue
-        if codeNum[:3]=='002':
-            continue
-        if codeNum[:3]=='730':
-            continue
-        if codeNum[:3]=='700':
-            continue 
-        if codeNum[:3]=='080':
-            continue 
-        if codeNum[:3]=='580':
-            continue 
-        if codeNum[:3]=='031':
-            continue 
-        if codeNum[:3]=='400':
-            continue 
+        #if codeNum[:3]=='300':
+            #continue
+        #if codeNum[:3]=='200':
+            #continue
+        #if codeNum[:3]=='900':
+            #continue
+        #if codeNum[:3]=='002':
+            #continue
+        #if codeNum[:3]=='730':
+            #continue
+        #if codeNum[:3]=='700':
+            #continue 
+        #if codeNum[:3]=='080':
+            #continue 
+        #if codeNum[:3]=='580':
+            #continue 
+        #if codeNum[:3]=='031':
+            #continue 
+        #if codeNum[:3]=='400':
+            #continue 
         theRate = (currentDayAvg-lastDayClose)/lastDayClose
         if theRate>potentialRate[0] and theRate<potentialRate[1]:
             if codeHead=='sh':
                 potentialList[codeNum+'.XSHG']=theRate
+                #df = get_bars(codeNum+'.XSHG', 2, unit='1d',fields=['date','open','high','low','close','volume', 'money'],include_now=True)
+                #print(df)
             if codeHead=='sz':
                 potentialList[codeNum+'.XSHE']=theRate
 potentialList=sorted(potentialList.items(),key=lambda kv:(kv[1], kv[0]),reverse=False)
 #print(potentialList)
 print ('there are %d potential stocks.'%len(potentialList))
      
-authStatus = auth('15850798209','rbldevil198929aA')
-#authStatus = auth('13308189942','rbldevil198929aA')
-#authStatus = auth('15730085953','rbldevil198929aA')
-#authStatus = auth('18081550388','rbldevil198929aA')
-print("sdk count = ",get_query_count())
 
-#all_securities=get_all_securities(['stock'])
-#all_securities.to_csv('stocklist.csv', sep=',')
-all_securities=pd.read_csv('stocklist.csv', sep=',',index_col=[0])
 
 targetCodeId=[]
 all_securities_list=[]
@@ -91,7 +93,7 @@ for code in potentialList:
 
 endDate='' 
 nextStartDate=''
-if time.localtime().tm_hour<16:
+if time.localtime().tm_hour<15:
     endDate=str(datetime.date.today() -datetime.timedelta(days=1))
     nextStartDate=str(datetime.date.today())    
 else:
@@ -117,24 +119,23 @@ for codeId in targetCodeId:
         theBeginMonth = int(currentStartDate.split('-')[1])
         theBeginDay = int(currentStartDate.split('-')[2])
         if currentEndDate_year*12+currentEndDate_month-theBeginYear*12+theBeginMonth>=24:
-            currentStartDate = str(datetime.date.today() -datetime.timedelta(days=20))
+            currentStartDate = str(datetime.date.today() -datetime.timedelta(days=60))
         infoDict={'currentStartDate':currentStartDate};
         with open(info_doc_path,"w") as f: json.dump(infoDict,f)
-    print("start date = ",currentStartDate)
+    #print("start date = ",currentStartDate)
     currentStartDate_year =int(currentStartDate.split('-')[0])
     currentStartDate_month = int(currentStartDate.split('-')[1])
     currentStartDate_day = int(currentStartDate.split('-')[2])
     
     if  currentStartDate_year*375+31*currentStartDate_month+currentStartDate_day>\
         currentEndDate_year*375+31*currentEndDate_month+currentEndDate_day:
-        #print("StartDate : "+currentStartDate)
-        #print("endDate : "+endDate)
-        #print("date err")
+        #print("StartDate > endDate ")
         continue
     if currentEndDate_year==currentStartDate_year and currentEndDate_month==currentStartDate_month and currentEndDate_day==currentStartDate_day:
-        print("the same date not need updata")
+        #print("the same date not need updata")
         continue
     else:
+        print("PULL ... ")
         df_add = get_price(thisCode, start_date=currentStartDate, end_date=endDate+' 23:00:00', frequency='minute') 
         df_add.to_csv(dataFolder+'/'+str(thisCode)+'.csv', sep=',', mode='a', header=False)
         infoDict={'currentStartDate':nextStartDate};
